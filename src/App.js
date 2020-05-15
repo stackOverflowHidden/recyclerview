482229750353727db3c8f4c0a5f60558cba43d81
筱川 <suchuan.sc@alibaba-inc.com>
2020-05-15 16:32:48 +0800

    feat：翻页

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 /*
  * @Author: your name
  * @Date: 2020-05-10 17:38:52
- * @LastEditTime: 2020-05-11 00:11:19
+ * @LastEditTime: 2020-05-15 16:26:45
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /recyclerview/src/App.js
  */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import List from './mod/list/index';
 import mockData from "./mockData";
@@ -21,61 +21,75 @@ const listStyle = {
 
 function formatNum(param) {
   if (!param) return;
-  
+
   const num = Number(param);
   if (num < 10000) {
     return num;
   } else if (num >= 10000) {
-    return String((num/10000).toFixed(2)).replace(/.$/, "万");
+    return String((num / 10000).toFixed(2)).replace(/.$/, "万");
   }
 }
 
 function App() {
-  function renderItem(cardData, index) {
-    return <div
-    className="container"
-    key={cardData.id}
-    onClick={() => {
-      if (!cardData.contentURL) return;
+  const [dataSource, addDataSource] = useState([]);
 
-      window.open(cardData.contentURL);
-    }}
-  >
-    {
-      cardData.videoUrl ? <video controls className="coverImg" src={cardData.videoUrl} autoplay={true} /> : <img
-        src={cardData.cover}
-        className="coverImg"
-      />
-    }
-    {
-      cardData.idDraft !== true && cardData.idDraft !== "true" ? <div className="videoPlay">
-        <img
-          className="videoIcon"
-          src="https://gw.alicdn.com/tfs/TB16jJ3BkL0gK0jSZFxXXXWHVXa-30-32.png"
+  useEffect(() => {
+    addDataSource(mockData.slice());
+  }, []);
+
+  function renderItem(cardData = {}, index) {
+    return <div
+      className="container"
+      key={`${cardData.id}_${index}`}
+      onClick={() => {
+        if (!cardData.contentURL) return;
+
+        window.open(cardData.contentURL);
+      }}
+    >
+      {
+        cardData.videoUrl ? <video controls className="coverImg" src={cardData.videoUrl} autoPlay={true} /> : <img
+          src={cardData.cover}
+          className="coverImg"
         />
-        <span className="readCount">{formatNum(cardData.readCount)}</span>
-      </div> : null
-    }
-    <p className="title">{cardData.title}</p>
-    <div className="interactions">
-      <img className="comment" src="https://gw.alicdn.com/tfs/TB1WcYecZKfxu4jSZPfXXb3dXXa-60-58.png" />
-      <span className="interactionsText">{formatNum(cardData.commentCount)}</span>
-      <img className="favour" src="https://gw.alicdn.com/tfs/TB1V.d1BkL0gK0jSZFxXXXWHVXa-60-60.png" />
-      <span className="interactionsText">{formatNum(cardData.praiseCount)}</span>
+      }
+      {
+        cardData.idDraft !== true && cardData.idDraft !== "true" ? <div className="videoPlay">
+          <img
+            className="videoIcon"
+            src="https://gw.alicdn.com/tfs/TB16jJ3BkL0gK0jSZFxXXXWHVXa-30-32.png"
+          />
+          <span className="readCount">{formatNum(cardData.readCount)}</span>
+        </div> : null
+      }
+      <p className="title">{cardData.title}</p>
+      <div className="interactions">
+        <img className="comment" src="https://gw.alicdn.com/tfs/TB1WcYecZKfxu4jSZPfXXb3dXXa-60-58.png" />
+        <span className="interactionsText">{formatNum(cardData.commentCount)}</span>
+        <img className="favour" src="https://gw.alicdn.com/tfs/TB1V.d1BkL0gK0jSZFxXXXWHVXa-60-60.png" />
+        <span className="interactionsText">{formatNum(cardData.praiseCount)}</span>
+      </div>
+      <div className="borderBottom" />
     </div>
-    <div className="borderBottom" />
-  </div>
+  }
+
+  function loadMore() {
+    addDataSource(dataSource => [].concat([], dataSource, mockData));
   }
 
   return (
     <div className="App">
-      <header className="header">无尽流容器</header>
+      <header
+        className="header"
+      >
+        无尽流容器
+      </header>
       <List
-        dataSource={mockData}
+        dataSource={dataSource}
         renderItem={renderItem}
         style={listStyle}
+        onEndReach={loadMore}
       />
-      {/* <footer className="footer">zaijian</footer> */}
     </div>
   );
 }
--- src/mod/list/index.js
+++ src/mod/list/index.js
@@ -1,24 +1,47 @@
 /*
  * @Author: your name
  * @Date: 2020-05-10 18:14:58
- * @LastEditTime: 2020-05-10 23:34:09
+ * @LastEditTime: 2020-05-15 16:29:01
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /recyclerview/src/mod/list/index.js
  */
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.css';
 
 export default function List(props) {
-  const { style = {}, dataSource, renderItem } = props;
+  const { style = {}, dataSource, renderItem, onEndReach } = props;
+
+  useEffect(() => {
+    let observer;
+    if (onEndReach && Object.prototype.toString.apply(onEndReach) === "[object Function]") {
+      observer = new IntersectionObserver((entries) => {
+        if (entries[0].intersectionRatio <= 0) return;
+        
+        onEndReach();
+      });
+      const footer = document.querySelector(".endReachFooter");
+      observer.observe(footer);
+    }
+
+    return observer ? () => {
+      observer.disconnect();
+    } : null;
+  }, []);
+
   if (Object.prototype.toString.apply(dataSource) !== "[object Array]" || Object.prototype.toString.apply(renderItem) !== "[object Function]") {
     return;
   }
-  console.log(dataSource)
   
-  return <article className="container" style={style}>
+  return <article
+    className="container"
+    style={style}
+  >
     {
       dataSource.map((item, index) => renderItem(item, index))
+    }
+    {
+      dataSource && <div className="endReachFooter" style={{ width: "100%", height: "100px", }} />
     }
   </article>
 }
